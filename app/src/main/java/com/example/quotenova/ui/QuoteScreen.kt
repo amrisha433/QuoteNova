8895b58e68b6dd884a241fffe90dadface759808
amrishamaurya <amrishamaurya@gmail.com>
2026-06-14 13:59:01 +0530

    Added dark mode and UI improvements

--- app/src/main/java/com/example/quotenova/ui/QuoteScreen.kt
+++ app/src/main/java/com/example/quotenova/ui/QuoteScreen.kt
@@ -1,27 +1,49 @@
-package com.example.quotenova
+package com.example.quotenova.ui
 
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.LightMode
 import android.content.Intent
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.quotenova.QuoteData
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -29,39 +51,98 @@ fun QuoteScreen() {
 
     val context = LocalContext.current
 
-    var currentQuote by remember {
+    var isDarkMode by remember {
+        mutableStateOf(false)
+    }
+
+    val backgroundColor =
+        if (isDarkMode) Color(0xFF0F172A)
+        else Color(0xFFF8FAFC)
+
+    val cardColor =
+        if (isDarkMode) Color(0xFF1E293B)
+        else Color(0xFFFFFFFF)
+
+    val titleColor =
+        if (isDarkMode) Color.White
+        else Color(0xFF111827)
+
+    val subtitleColor =
+        if (isDarkMode) Color(0xFF94A3B8)
+        else Color(0xFF6B7280)
+
+    val quoteColor =
+        if (isDarkMode) Color(0xFF10B981)
+        else Color(0xFF0F766E)
+
+    val authorColor =
+        if (isDarkMode) Color(0xFFCBD5E1)
+        else Color(0xFF4B5563)
+
+    val buttonColor =
+        if (isDarkMode) Color(0xFF10B981)
+        else Color(0xFF0F766E)
+
+    val shareButtonColor =
+        if (isDarkMode) Color(0xFF334155)
+        else Color(0xFFE2E8F0)
+
+    var currentQuote by remember{
         mutableStateOf(
             QuoteData.listOfQuotes.random()
         )
     }
-
     Column(
-        modifier = Modifier
+        modifier = Modifier.Companion
             .fillMaxSize()
-            .background(Color(0xFFF3F4F6))
+            .background(backgroundColor)
             .padding(bottom = 100.dp),
 
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.Companion.CenterHorizontally
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+
+            IconButton(
+                onClick = {
+                    isDarkMode = !isDarkMode
+                }
+            ) {
+
+                Icon(
+                    imageVector =
+                        if (isDarkMode)
+                            Icons.Default.LightMode
+                        else
+                            Icons.Default.DarkMode,
+                    contentDescription = "Theme Toggle",
+                    tint = buttonColor
+                )
+            }
+        }
 
         Text(
             text = "QuoteNova",
             fontSize = 42.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color(0xFF111827)
+            fontWeight = FontWeight.Companion.Bold,
+            color = titleColor
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.Companion.height(16.dp))
         Text(
             text = "Daily inspiration for your journey",
-            color = Color(0xFF6B7280),
+            color = subtitleColor,
             fontSize = 17.sp
         )
 
-        Spacer(modifier = Modifier.height(50.dp))
+        Spacer(modifier = Modifier.Companion.height(50.dp))
 
         Card(
-            modifier = Modifier
+            modifier = Modifier.Companion
                 .fillMaxWidth(0.9f),
 
             shape = RoundedCornerShape(24.dp),
@@ -71,7 +152,7 @@ fun QuoteScreen() {
             ),
 
             colors = CardDefaults.cardColors(
-                containerColor = Color(0xFF1F2937)
+                containerColor = cardColor
             )
         ) {
 
@@ -87,29 +168,29 @@ fun QuoteScreen() {
 
                     Text(
                         text = quote.text,
-                        modifier = Modifier.padding(20.dp),
+                        modifier = Modifier.Companion.padding(20.dp),
                         fontSize = 24.sp,
-                        color = Color(0xFFFBBF24)
+                        color = quoteColor
                     )
 
                     Text(
                         text = "- ${quote.author}",
-                        modifier = Modifier.padding(
+                        modifier = Modifier.Companion.padding(
                             start = 100.dp,
                             end = 100.dp,
                             bottom = 20.dp
                         ),
                         fontSize = 18.sp,
-                        color = Color(0xFFD1D5DB)
+                        color = authorColor
                     )
                 }
             }
         }
 
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.Companion.height(40.dp))
 
         Row(
-            verticalAlignment = Alignment.CenterVertically,
+            verticalAlignment = Alignment.Companion.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
 
@@ -125,19 +206,19 @@ fun QuoteScreen() {
                         currentQuote = availableQuotes.random()
                     }
                 },
-                modifier = Modifier.size(180.dp, 50.dp),
+                modifier = Modifier.Companion.size(180.dp, 50.dp),
 
-                shape = RoundedCornerShape(18.dp),
+                shape = androidx.compose.foundation.shape.RoundedCornerShape(18.dp),
 
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFF374151),
-                    contentColor = Color(0xFFFBBF24)
+                    containerColor = buttonColor,
+                    contentColor = Color.White
                 )
             ) {
 
                 Text(
                     text = "Next Quote",
-                    fontWeight = FontWeight.SemiBold,
+                    fontWeight = FontWeight.Companion.SemiBold,
                     fontSize = 18.sp
                 )
             }
@@ -162,10 +243,10 @@ fun QuoteScreen() {
                     )
                 },
 
-                modifier = Modifier
+                modifier = Modifier.Companion
                     .size(48.dp)
                     .background(
-                        Color(0xFF374151),
+                        shareButtonColor,
                         CircleShape
                     )
             ) {
@@ -173,7 +254,7 @@ fun QuoteScreen() {
                 Icon(
                     imageVector = Icons.Default.Share,
                     contentDescription = "Share Quote",
-                    tint = Color(0xFFFBBF24)
+                    tint = buttonColor
                 )
             }
         }
